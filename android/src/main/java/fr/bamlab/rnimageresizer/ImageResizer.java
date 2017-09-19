da1c174a4477cb64bb2ec2583d7511cceef44a31
Y.Kulikov <UKulikov@yandex.ru>
2017-09-19 14:31:56 +0300

    commented rotation

--- android/src/main/java/fr/bamlab/rnimageresizer/ImageResizer.java
+++ android/src/main/java/fr/bamlab/rnimageresizer/ImageResizer.java
@@ -288,7 +288,7 @@ class ImageResizer {
         }
 
         // Rotate if necessary
-        Bitmap rotatedImage = ImageResizer.rotateImage(scaledImage, rotation);
+        // Bitmap rotatedImage = ImageResizer.rotateImage(scaledImage, rotation);
 
         if (scaledImage != rotatedImage) {
             scaledImage.recycle();
